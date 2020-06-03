f4e78ff6c205caed5144c60d0fa40574ffb8757f
Adil <adil.moen@gmail.com>
2020-06-03 17:17:19 -0400

    fizzbuzz

--- app/src/main/java/com/appsbymoen/fizzbuzzme/MainActivity.kt
+++ app/src/main/java/com/appsbymoen/fizzbuzzme/MainActivity.kt
@@ -11,11 +11,20 @@ class MainActivity : AppCompatActivity() {
 
         for (i in 0..100) {
 
-        when {
-            i<50 -> Log.i(i.toString(),"fizz")
-            i>75 -> Log.i(i.toString(),"buzz")
-            else -> Log.i(i.toString(),"fizz buzz")
-        }
+//        when {
+//            i<50 -> Log.i(i.toString(),"fizz")
+//            i>75 -> Log.i(i.toString(),"buzz")
+//            else -> Log.i(i.toString(),"fizz buzz")
+//        }
+
+            if (i % 15 ==0) {
+                Log.i("fizzBuzz", i.toString())
+            } else if (i % 3 == 0) {
+                    Log.i("fizz", i.toString())
+            } else if (i % 5 == 0) {
+                    Log.i("buzz", i.toString())
+            }
+
         }
 
     }
