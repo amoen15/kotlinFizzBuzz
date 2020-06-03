06ce546b2461fd8c33525cab2b0c7aa07055e1e3
Adil <adil.moen@gmail.com>
2020-06-03 16:00:36 -0400

    while

--- app/src/main/java/com/appsbymoen/fizzbuzzme/MainActivity.kt
+++ app/src/main/java/com/appsbymoen/fizzbuzzme/MainActivity.kt
@@ -2,10 +2,21 @@ package com.appsbymoen.fizzbuzzme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        for (i in 0..100) {
+
+        when {
+            i<50 -> Log.i(i.toString(),"fizz")
+            i>75 -> Log.i(i.toString(),"buzz")
+            else -> Log.i(i.toString(),"fizz buzz")
+        }
+        }
+
     }
 }
